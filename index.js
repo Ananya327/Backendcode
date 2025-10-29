71bf0b6bc2d1865af4f0c7ff5aaa9180161a8221
AnanyaNaik <naikananya02@gmail.com>
2025-10-29 10:07:45 +0530

    add thefiles are added

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ import express, { urlencoded } from "express";
 import connectDB from "./src/helper/database.js";
 import router from "./routes.js";
 import dotenv from "dotenv";
-dotenv.config()
+// dotenv.config();
+dotenv.config({ quiet: true });
 const app = express();
-
 
 // allow json  format and accect urlencoder
 app.use(express.json());
@@ -15,6 +15,6 @@ app.use(express.urlencoded({ extended: true }));
 connectDB();
 router(app);
 
-app.listen(process.env.PROT, () => {
-  console.log("server listening to PORT ", 3000);
+app.listen(process.env.PORT, () => {
+  console.log("server listening to PORT ", process.env.PORT);
 });
