417503c2029d4ced42035d1d6463def4ccf66336
AnanyaNaik <naikananya02@gmail.com>
2025-10-29 10:06:18 +0530

    requried field are added

--- src/controller/managestudent/createStudent.js
+++ src/controller/managestudent/createStudent.js
@@ -4,7 +4,29 @@ const router = Router();
 
 export default router.post("/", async (req, res) => {
   try {
-    let { name, email, rollno ,usn} = req.body || {};
+    let { name, email, rollno, usn } = req.body || {};
+
+    // to define  requried fields
+    if (!name || name == undefined) {
+      return res.send({
+        message: "name requried",
+      });
+    }
+    if (!email || email == undefined) {
+      return res.send({
+        message: "email requried",
+      });
+    }
+    if (!rollno || rollno == undefined) {
+      return res.send({
+        message: "rollno requried",
+      });
+    }
+    if (!usn || usn == undefined) {
+      return res.send({
+        message: "usn requried",
+      });
+    }
     await studentModule.create({
       //     ...req.body
       name,
